916ad15bd8b1259d1efd5880837f54377c01d033
Almaz <atunstyle@gmail.com>
2020-12-23 00:37:36 +0300

    refactor and add component User *Improve a tittle bit style Home component *Add method to get users from server in the Home component

---
+++ src/components/User.vue
@@ -0,0 +1,16 @@
+<template>
+  <router-link :to="{name: 'AlbumsOfUser', params: {userId: idUser}}">
+    <div class="box-content h-80 w-50 p-4 border-2 rounded-lg hover:shadow-xl xl:transition-shadow ease-in-out">
+      <div>
+        <span>{{ userName }}</span>
+      </div>
+    </div>
+  </router-link>
+</template>
+
+<script>
+export default {
+  name: "User",
+  props: ['userName', 'idUser'],
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,36 @@
 <template>
-  <div class="flex flex-col justify-center content-center items-center">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container grid grid-cols-5 gap-4 mx-auto my-10 min-h-3/4">
+    <div v-for="user in users" :key="user.id">
+      <User :user-name="user.name" :id-user="user.id"/>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import {mapGetters} from "vuex";
+import User from "@/components/User";
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    User: User
+  },
+  computed: {
+    // mix the getters into computed with object spread operator
+    ...mapGetters({
+      users:  'users',
+    })
+  },
+  methods: {
+    getUsers() {
+      this.$store.dispatch({
+        type: 'getUsersJSON'
+      });
+    },
+  },
+  created(){
+    this.getUsers()
+    console.log('render')
   }
 }
 </script>
