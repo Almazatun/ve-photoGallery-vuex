b6de3a1d3bc88e3789731800a33dadc079ab03f9
Almaz <atunstyle@gmail.com>
2020-12-22 23:59:35 +0300

    add Loader component

---
+++ src/components/common/Loader/Loader.vue
@@ -0,0 +1,24 @@
+<template>
+  <div :style="divStyle"> <img src="./Loader.svg" alt="loader"> </div>
+</template>
+
+<script>
+export default {
+  name: "Loader",
+  computed: {
+    divStyle() {
+      return {
+        position: 'absolute',
+        height: '250px',
+        left: '50%',
+        top: '50%',
+        transform: 'translate(-50%, -50%)',
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
