1613a1545b220e20e37e6815b6ccbfe4cad8b4ca
Almaz <atunstyle@gmail.com>
2020-12-23 00:02:46 +0300

    refactor App file *add condition rendering block

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,32 @@
 <template>
-  <div id="nav" class="flex justify-center text-green-800 text-5xl font-bold">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about"> About</router-link>
+  <div class="bg-blue-100 bg-opacity-10 h-screen min-h-screen" v-if="!isFetch">
+    <Routes/>
+    <router-view/>
   </div>
-  <router-view/>
+  <div v-else>
+    <Loader/>
+  </div>
 </template>
 
-<style>
-</style>
+<script>
+import Routes from "@/components/Routes";
+import {useStore} from "vuex";
+import Loader from "@/components/common/Loader/Loader";
+import {ref} from 'vue'
+
+export default {
+  components: {
+    Routes: Routes,
+    Loader: Loader
+  },
+  setup() {
+    const store = useStore()
+    const isFetch = ref(store.getters.isFetch)
+    console.log(isFetch)
+
+    return {
+      isFetch
+    }
+  }
+}
+</script>
