ab8e5ef950b0fc8569b7bbeb03bd75a6d5adaf7e
Almaz <atunstyle@gmail.com>
2020-12-22 22:27:08 +0300

    :alien:add restfull API | method only GET *getUsers *getUserAlbums *getPhotosAlbumOfUser

--- src/api/ap1.js
+++ src/api/ap1.js
@@ -1,5 +1,13 @@
+import {instance} from "@/util/axios";
+
 export const API = {
-    saYHello () {
-        alert('API')
+    getUsers() {
+        return instance.get(`/users`).then(response => response.data)
+    },
+    getUserAlbums(userId){
+        return instance.get(`/users/${userId}/albums`).then(response => response.data)
+    },
+    getAlbumPhotosOfUser(albumNumber){
+        return instance.get(`/albums/${albumNumber}/photos`).then(response => response.data)
     }
 }
