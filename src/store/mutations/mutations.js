3409128daf9bfa0d78ceb2eb37f078d88f574b2a
Almaz <atunstyle@gmail.com>
2020-12-23 11:57:59 +0300

    add new mutation method *Method to toggle isFetching value in the store when receiving data from a server

--- src/store/mutations/mutations.js
+++ src/store/mutations/mutations.js
@@ -1,4 +1,9 @@
-import {SET_PHOTOS_FROM_ALBUM_OF_USER, SET_USER_ALBUMS, SET_USERS} from "@/utils/actions_mutation";
+import {
+    IS_FETCHING_DATA_FROM_SERVER,
+    SET_PHOTOS_FROM_ALBUM_OF_USER,
+    SET_USER_ALBUMS,
+    SET_USERS
+} from "@/utils/actions_mutation";
 
 export const MUTATIONS = {
     [SET_USERS](state, payload) {
@@ -9,5 +14,8 @@ export const MUTATIONS = {
     },
     [SET_PHOTOS_FROM_ALBUM_OF_USER](state, payload){
         state.userAlbumPhotos = payload
+    },
+    [IS_FETCHING_DATA_FROM_SERVER](state, payload){
+        state.isFetching = payload
     }
 }
