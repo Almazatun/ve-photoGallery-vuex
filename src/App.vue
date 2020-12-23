f04664704a1144e91c951d2787f2512ead04711e
Almaz <atunstyle@gmail.com>
2020-12-23 12:19:17 +0300

    :wastebasket:delete condition render block

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,16 @@
 <template>
-  <div class="bg-blue-100 bg-opacity-10 h-screen min-h-screen" v-if="!isFetch">
+  <div class="bg-blue-100 bg-opacity-10 h-screen min-h-screen">
     <Routes/>
     <router-view/>
-  </div>
-  <div v-else>
-    <Loader/>
   </div>
 </template>
 
 <script>
 import Routes from "@/components/Routes";
-import {useStore} from "vuex";
-import Loader from "@/components/common/Loader/Loader";
-import {ref} from 'vue'
 
 export default {
   components: {
     Routes: Routes,
-    Loader: Loader
-  },
-  setup() {
-    const store = useStore()
-    const isFetch = ref(store.getters.isFetch)
-    console.log(isFetch)
-
-    return {
-      isFetch
-    }
   }
 }
 </script>
