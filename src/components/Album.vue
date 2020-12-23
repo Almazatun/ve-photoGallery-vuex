a9a3760d7212924b32cbff143d64b34639b31c55
Almaz <atunstyle@gmail.com>
2020-12-23 14:58:27 +0300

    :truck:rewrite navigation routers *Using useRouter

--- src/components/Album.vue
+++ src/components/Album.vue
@@ -1,16 +1,18 @@
 <template>
   <div v-for="album in userAlbums" :key="album.id">
-    <router-link :to="{name: 'AlbumPhotos', params: {userId: album.userId, albumId: album.id}}"> Redirect </router-link>
-      <div class="box-content h-50 w-50 p-4 border-2 rounded-lg hover:shadow-xl xl:transition-shadow ease-in-out
+    <div class="box-content h-50 w-50 p-4 border-2 rounded-lg hover:shadow-xl xl:transition-shadow ease-in-out
                 bg-clip-padding p-6 bg-indigo-600 border-4 border-indigo-300 border-dashed
-                ">
-        <span class="font-sans">{{ album.title }}</span>
-      </div>
+                cursor-pointer
+                " @click="goParticularAlbumPhoto(album.userId, album.id)">
+      <span class="font-sans">{{ album.title }}</span>
+    </div>
   </div>
 
 </template>
 
 <script>
+import {useRouter} from "vue-router";
+
 export default {
   name: "Album",
   props: {
@@ -19,9 +21,16 @@ export default {
       required: true
     }
   },
+  setup() {
+    const router = useRouter()
+
+    function goParticularAlbumPhoto(userId, albumId) {
+      router.push({name: 'AlbumPhotos', params: {userId: userId, albumId: albumId}})
+    }
+
+    return {
+      goParticularAlbumPhoto
+    }
+  }
 }
 </script>
-<!--/*params: {albumId: album.id}}*/-->
-<style scoped>
-
-</style>
--- src/components/User.vue
+++ src/components/User.vue
@@ -1,16 +1,31 @@
 <template>
-  <router-link :to="{name: 'AlbumsOfUser', params: {userId: idUser}}">
-    <div class="box-content h-80 w-50 p-4 border-2 rounded-lg hover:shadow-xl xl:transition-shadow ease-in-out">
-      <div>
-        <span>{{ userName }}</span>
-      </div>
+  <div class="box-content h-80 w-50 p-4 border-2 rounded-lg hover:shadow-xl xl:transition-shadow ease-in-out
+                cursor-pointer
+"
+       @click="goAlbumsOfUser(idUser)"
+  >
+    <div>
+      <span>{{ userName }}</span>
     </div>
-  </router-link>
+  </div>
 </template>
 
 <script>
+import {useRouter} from "vue-router";
+
 export default {
   name: "User",
   props: ['userName', 'idUser'],
+  setup() {
+    const router = useRouter()
+
+    function goAlbumsOfUser(userId) {
+      router.push({name: 'AlbumsOfUser', params: {userId: userId}})
+    }
+
+    return {
+      goAlbumsOfUser
+    }
+  }
 }
 </script>
