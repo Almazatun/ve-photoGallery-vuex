ec2d5b3d5ecaedd41002d0a3f68b308a7783fc01
Almaz <atunstyle@gmail.com>
2020-12-23 12:18:07 +0300

    include new mutation method inside all actions *The mutation method does toggle isFetching value in the store

--- src/store/actions/actions.js
+++ src/store/actions/actions.js
@@ -1,33 +1,38 @@
 import {API} from "@/api/ap1";
-import {SET_PHOTOS_FROM_ALBUM_OF_USER, SET_USER_ALBUMS, SET_USERS} from "@/utils/actions_mutation";
+import {
+    IS_FETCHING_DATA_FROM_SERVER,
+    SET_PHOTOS_FROM_ALBUM_OF_USER,
+    SET_USER_ALBUMS,
+    SET_USERS
+} from "@/utils/actions_mutation";
 
 export const ACTIONS = {
     async getUsersJSON({commit}) {
         try {
-            //loader true
+            commit(IS_FETCHING_DATA_FROM_SERVER, true)
             const usersData = await API.getUsers()
             commit(SET_USERS, usersData)
-            //loader false
+            commit(IS_FETCHING_DATA_FROM_SERVER, false)
         } catch (error) {
             alert(error)
         }
     },
     async getUserAlbumsJSON({commit}, payload){
         try {
-            //loader true
+            commit(IS_FETCHING_DATA_FROM_SERVER, true)
             const userAlbums = await API.getUserAlbums(payload.userId)
             commit(SET_USER_ALBUMS, userAlbums)
-            //loader false
+            commit(IS_FETCHING_DATA_FROM_SERVER, false)
         } catch (error) {
             alert(error)
         }
     },
     async getAlbumPhotosOfUserJSON({commit}, payload){
         try {
-            //loader true
+            commit(IS_FETCHING_DATA_FROM_SERVER, true)
             const photos = await API.getAlbumPhotosOfUser(payload.albumId)
             commit(SET_PHOTOS_FROM_ALBUM_OF_USER, photos)
-            //loader false
+            commit(IS_FETCHING_DATA_FROM_SERVER, false)
         } catch (error) {
             alert(error)
         }
