a6d1ede6843cd62cee5f459533ba12b658f127e0
Almaz <atunstyle@gmail.com>
2020-12-24 22:55:19 +0300

    add colors state in the store

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,29 @@
-import { createStore } from 'vuex'
+import {createStore} from 'vuex'
 import {ACTIONS} from './actions/actions'
 import {MUTATIONS} from "@/store/mutations/mutations";
 import {GETTERS} from "@/store/getters/getters";
 
 export default createStore({
-  state: {
-    users: [],
-    userAlbums: [],
-    userAlbumPhotos: [],
-    isFetching: false
-  },
-  mutations: MUTATIONS,
-  actions: ACTIONS,
-  getters: GETTERS,
-  modules: {}
+    state: {
+        colors: [
+            {id: '1', color: '#f94144'},
+            {id: '2', color: '#f3722c'},
+            {id: '3', color: '#f8961e'},
+            {id: '4', color: '#f9c74f'},
+            {id: '5', color: '#90be6d'},
+            {id: '6', color: '#43aa8b'},
+            {id: '7', color: '#577590'},
+            {id: '8', color: "#355070"},
+            {id: '9', color: "#6d597a"},
+            {id: '10', color: "#b56576"},
+        ],
+        users: [],
+        userAlbums: [],
+        userAlbumPhotos: [],
+        isFetching: false
+    },
+    mutations: MUTATIONS,
+    actions: ACTIONS,
+    getters: GETTERS,
+    modules: {}
 })
