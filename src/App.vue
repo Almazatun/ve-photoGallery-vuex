fa5df251c48f385f9c4f87c3facbf12cfe4abbc5
Almaz <atunstyle@gmail.com>
2020-12-25 12:22:41 +0300

    :wastebasket:delete unused components and router *Deleted About page router and component *Deleted HelloWorld.vue component *Deleted Routes.vue component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,10 @@
 <template>
   <div class="bg-blue-100 bg-opacity-10 h-screen min-h-screen">
-    <Routes/>
     <router-view/>
   </div>
 </template>
 
 <script>
-import Routes from "@/components/Routes";
 
-export default {
-  components: {
-    Routes: Routes,
-  }
-}
+export default {}
 </script>
