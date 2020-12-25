c06445f7aa90d86ba496437d09d58c83e925f712
Almaz <atunstyle@gmail.com>
2020-12-26 00:54:20 +0300

    :iphone:modify code and add features,responsive design *Delete v-for render list option from PhotoFrame.vue component. *Add button on the Photos page to get back the Albums page of user. *Improve UX on the Photos page. When will be making a request from the Albums page to the server for getting albums data of the user, there will appear ajax loader on the page. *Modify css styles and add responsive design

--- src/components/PhotoFrame.vue
+++ src/components/PhotoFrame.vue
@@ -1,15 +1,18 @@
 <template>
-  <div v-for="photo in photos" :key="photo.id">
-    <span>{{ photo.title }}</span>
-  </div>
+  <div class="box-content h-80 w-50 p-4 bg-center bg-cover rounded-2xl transition duration-150 ease-in-out mb-5"
+       :style="{backgroundImage: `url(${photoUrl})`}"></div>
 </template>
 
 <script>
 export default {
   name: "PhotoFrame",
   props: {
-    photos: {
-      type: Array,
+    photoUrl: {
+      type: String,
+      required: true
+    },
+    photoTitle: {
+      type: String,
       required: true
     }
   }
--- src/views/Photos.vue
+++ src/views/Photos.vue
@@ -1,9 +1,29 @@
 <template>
-  <div>
-    <span>Photos</span>
-    <span>{{`Album: ${albumId}`}}</span>
-    <div >
-      <PhotoFrame :photos="photos"/>
+  <div v-if="isFetching">
+    <Loader/>
+  </div>
+  <div class="container flex flex-col hover:opacity-100" style="margin: auto" v-else>
+    <span class="bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-blue-500 font-bold
+                font-sans text-4xl mt-5 text-center">Photos</span>
+    <div class="ml-2.5">
+      <button @click="goBackPreviousPage()"
+              class="font-sans font-bold hover:bg ease-in-out h-20 w-20 bg-gray-100 rounded text-center">
+        <img class="inline-block align-middle"
+             height="35"
+             width="35"
+             src="https://image.flaticon.com/icons/png/128/271/271220.png"
+             alt="arrow icon">
+      </button>
+    </div>
+    <div class="grid lg:grid-cols-5 md:grid-cols-3 sm:grid-cols-2 p-12 gap-4 mx-auto my-10 min-h-3/4">
+      <div v-for="photo in photos" :key="photo.id">
+        <PhotoFrame :photo-title="photo.title" :photo-url="photo.url"/>
+        <div class="ml-4">
+          <span class="font-bold">Photo title: </span>
+          <br>
+          <span class="break-words font-sans text-left"> {{ photo.title }} </span>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -11,19 +31,28 @@
 <script>
 import PhotoFrame from "@/components/PhotoFrame";
 import {mapGetters} from "vuex";
-import {useRoute} from "vue-router";
+import {useRoute, useRouter} from "vue-router";
+import Loader from "@/components/common/Loader/Loader";
 
 export default {
   name: "Photos",
   components: {
+    Loader: Loader,
     PhotoFrame: PhotoFrame
   },
 
-  setup(){
-    const { params: { albumId } } = useRoute()
+  setup() {
+    const {params: {albumId}} = useRoute()
+    const {params: {userId}} = useRoute()
+    const router = useRouter()
+
+    function goBackPreviousPage() {
+      router.push({name: 'AlbumsOfUser', params: {userId: userId}})
+    }
 
     return {
-      albumId
+      albumId,
+      goBackPreviousPage
     }
   },
 
@@ -35,12 +64,13 @@ export default {
       });
     },
   },
-  created(){
+  created() {
     this.getPhotos()
   },
   computed: {
     ...mapGetters({
-      photos:  'userAlbumPhotos',
+      photos: 'userAlbumPhotos',
+      isFetching: 'isFetch'
     })
   },
 }
